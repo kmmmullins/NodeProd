80f85a95bfe22e3e202e18599cfd9fed84c10a03
kmmmullins <32374231+kmmmullins@users.noreply.github.com>
2017-09-30 10:55:28 -0400

    Added Files

---
+++ config.js
@@ -0,0 +1,8 @@
+
+module.export = {
+    consumer_key: 'PFgAqz5DEMDAGsiYvRSHMi4af',
+    consumer_secret: '903fvmrazin8LUPdVSvimRy01HIjBHWJdMynoA9AX8zdGsApVP',
+    access_token: '7965752-g0xxVGMVnt1scGjwNK5wOF4sQVrkfwaLyfuABHlbIr',
+    access_token_secret: 'hkcfBSAMbPKiXlt2jJAKxMZB3Z0buleqsgbNTJ8pfhC7P'
+}
+
